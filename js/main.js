8e6971232c5a3bb7fecf5fb6ad557fc742eff136
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-01-13 23:48:06 +0200

    update app

--- js/main.js
+++ js/main.js
@@ -18,3 +18,15 @@ myBullets.forEach(function(ele){
 })
 
 
+
+
+// Swiper
+
+ var swiper = new Swiper(".mySwiper", {
+   slidesPerView: 3,
+   spaceBetween: 30,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+ });
