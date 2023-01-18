9f874e663d59dcaaa62d4a17b2b3a66dd7e1842d
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-01-18 16:05:20 +0200

    update app

--- js/main.js
+++ js/main.js
@@ -3,36 +3,42 @@ let myText = document.querySelectorAll(".text-wrapper h1");
 
 let navOffset = document.querySelector("header .navbar");
 
-
-
-
 //Remove Class Active From Bullets
 
-myBullets.forEach(function(ele){
-  ele.onclick = function(){
-    myBullets.forEach(function(e){
+myBullets.forEach(function (ele) {
+  ele.onclick = function () {
+    myBullets.forEach(function (e) {
       e.classList.remove("active_Bullet");
     });
     this.classList.add("active_Bullet");
-  }
-})
-
-
-
+  };
+});
 
 // Swiper
 
- var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 3,
-   spaceBetween: 25,
-   slidesPerGroup: 3,
-   loop: true,
-   fade: 'true',
-   grabCursor: 'true',
-   loopFillGroupWithBlank: true,
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  slidesPerGroup: 3,
+  loop: true,
+  fade: "true",
+  grabCursor: "true",
+  loopFillGroupWithBlank: true,
 
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
- });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
